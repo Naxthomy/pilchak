7e89b424b502004f9b45b287103d40ed6f8f45e6
naxthomy <naxthomy1@gmail.com>
2026-09-10 04:35:59 -0300

    Añadir estilos

--- src/views/_layouts/auth.layout.php
+++ src/views/_layouts/auth.layout.php
@@ -12,6 +12,7 @@ if (isset($_SESSION['user'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href=<?= '/assets/css/bootstrap.min.css' ?> >
+  <link rel="stylesheet" href="/assets/css/styles.css">
   <script src=<?= '/assets/js/bootstrap.min.js' ?>></script>
   <title>Autenticate - PDI</title>
 </head>
--- src/views/_layouts/layout.php
+++ src/views/_layouts/layout.php
@@ -20,6 +20,7 @@ function logout() {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href=<?= '/assets/css/bootstrap.min.css' ?> >
+  <link rel="stylesheet" href="/assets/css/styles.css">
   <script src=<?= '/assets/js/bootstrap.min.js' ?>></script>
   <title>App</title>
 </head>
